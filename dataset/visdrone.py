fix: fix image scaling with propoer bbox changes add XML previewer

dataset/visdrone.py
@@ -1,6 +1,7 @@
 from glob import glob
 import os
 from pathlib import Path
+from tools.utils import read_annotation_file
 import torch
 import torchvision.transforms.v2
 from torch.utils.data.dataset import Dataset
@@ -37,49 +38,15 @@ def load_images_and_anns(im_sets, label2idx, ann_fname):
             im_dir = os.path.join(im_set, 'ResizedSequences', vid)
             for (_, _, filenames) in os.walk(os.path.join(im_set, "SequenceAnnotations", vid)):
                 for _, ann_file in enumerate(filenames):
-                    im_info = {}
-                    ann_info = ET.parse(os.path.join(im_set, "SequenceAnnotations", vid, ann_file))
-                    root = ann_info.getroot()
-                    size = root.find('size')
-                    width = int(size.find('width').text)
-                    height = int(size.find('height').text)
-                    im_info['img_id'] = os.path.basename(ann_file).split('.xml')[0]
-                    im_info['filename'] = os.path.join(
-                        im_dir, '{}.jpg'.format(im_info['img_id'])
-                    )
-                    im_info['width'] = width
-                    im_info['height'] = height
-                    detections = []
-
-                    for obj in ann_info.findall('object'):
-                        det = {}
-                        difficult = int(obj.find('truncated').text)
-                        bbox_info = obj.find('bndbox')
-                        bbox = [
-                            int(bbox_info.find('xmin').text) - 1,
-                            int(bbox_info.find('ymin').text) - 1,
-                            int(bbox_info.find('xmax').text) - 1,
-                            int(bbox_info.find('ymax').text) - 1
-                        ]
-                        det['bbox'] = bbox
-                        det['difficult'] = difficult
-                        try:
-                            label = label2idx[obj.find('name').text]
-                            det['label'] = label
-                            if label == 0:
-                                print('Found background label for object {} in image {}. Skipping...'.format(ET.tostring(obj, encoding='unicode'), im_info['filename']))
-                                continue
-                        except KeyError:
-                            continue
-                        
-                        # At test time eval does the job of ignoring difficult
-                        detections.append(det)
+                    ann_dir = os.path.join(im_set, "SequenceAnnotations", vid)
+                    im_info, success = read_annotation_file(ann_dir, im_dir, ann_file, label2idx)
+                    if not success:
+                        continue
 
                     # Skip images with no detections
-                    if len(detections) == 0:
+                    if len(im_info.get('detections', [])) == 0:
                         continue
                     
-                    im_info['detections'] = detections
                     im_infos.append(im_info)
                 # Iterate for every annotation file
             # Iterating over sequence annotations

tools/train.py
@@ -7,11 +7,9 @@ import random
 from tqdm import tqdm
 from dataset.visdrone import VisDroneDataset
 from model.ssd import SSD
-import torchvision
 from dataset.voc import VOCDataset
 from torch.utils.data.dataloader import DataLoader
 from torch.optim.lr_scheduler import MultiStepLR
-from torch.cuda.amp import GradScaler, autocast
 
 if not torch.cuda.is_available():
     raise Exception('CUDA not available')
@@ -59,7 +57,8 @@ def train(args):
                                num_workers=8,  # 0 - 1 process, 4 or 8 - number of processes
                                pin_memory=True,  # Add this for faster GPU transfer
                                persistent_workers=True, # Keep workers alive between epochs
-                               prefetch_factor=2)  # Prefetch 2 batches per worker
+                               prefetch_factor=2  # Prefetch 2 batches per worker
+                               ) 
 
     # Instantiate model and load checkpoint if present
     model = SSD(config=config['model_params'],
@@ -135,12 +134,12 @@ def train(args):
             steps += 1
         optimizer.step()
         optimizer.zero_grad()
-        lr_scheduler.step()
+        lr_scheduler.step(i)
+        print('Learning rate for epoch {}: {:.6f}'.format(i+1, lr_scheduler.get_last_lr()[0]))
         epoch_time = time.time() - epoch_start_time
-        epoch_minutes = epoch_time / 60.0
-        epoch_hours = epoch_minutes / 60.0
+        epoch_minutes = epoch_time / 60 / 60
         print('Finished epoch {}/{}'.format(i+1, num_epochs))
-        print('Epoch execution time: {:.2f} hours, {:.2f} minutes'.format(epoch_hours, epoch_minutes))
+        print('Epoch execution time: {:.2f} minutes'.format(epoch_minutes))
         loss_output = ''
         loss_output += 'SSD Classification Loss : {:.4f}'.format(np.mean(ssd_classification_losses))
         loss_output += ' | SSD Localization Loss : {:.4f}'.format(np.mean(ssd_localization_losses))